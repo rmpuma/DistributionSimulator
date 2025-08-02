8a434e26b202f19bb2e993091d8f394002ca1622
rmpuma <99313810+rmpuma@users.noreply.github.com>
2025-08-02 07:35:53 -0600

    Update 05_factors.py

--- 05_factors.py
+++ 05_factors.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Oct  4 09:43:58 2023
-
-@author: Meva
-"""
-
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -21,4 +14,5 @@ factors = ['^SPX','IVW','IVE','QUAL','MTUM','SIZE','USMV',\
            'XLK','XLF','XLV','XLP','XLY','XLI','XLC','XLU']
 
 # compute factors
+
 df = capm.dataframe_factors(security, factors)
